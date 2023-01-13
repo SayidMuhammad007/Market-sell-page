215a2232e02f1ca2c8052999c6b62d4aa5be52e4
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2023-01-13 21:33:44 +0500

    all

--- myfiles/urls.py
+++ myfiles/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', Main, name='main'),
     path('sell/<id>', Amount_page, name='amount_page'),
     path('sell_page/', Sell_page, name='sell_page'),
-    path('sell_delete/<id>/', Sell_delete, name='sell_delete')
+    path('sell_delete/<id>/', Sell_delete, name='sell_delete'),
+    path('sell_price/<id>/', Sell_price, name='sell_price'),
+    path('selled/<type>', Selled, name='selled'),
+    path('nasiya', Customer_page, name='customer_page'),
+    path('nasiya/add', Customer_add, name='customer_add')
 ]
--- myfiles/views.py
+++ myfiles/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 import requests
-
+import json
 # from django.contrib import messages
 # Create your views here.
-IP_ADDRESS = 'http://192.168.88.94:8002' 
+IP_ADDRESS = 'http://127.0.0.1:8000' 
 payload = ""
 headers = {}
 
@@ -19,6 +19,7 @@ def Main(request):
     response = requests.request("GET", url, headers=headers, data=payload)
     for i in response.json():
         o = (i['all_pro'])
+        break
 
 
     return render(request, 'index.html', {'amount':response.json(), 'o':o})        
@@ -41,7 +42,6 @@ def Amount_page(request, id):
             return redirect('/') 
         
     else:
-        print('elsega kirdi')
         url = IP_ADDRESS+"/products?id="+id+"&name=*" 
 
 
@@ -52,17 +52,76 @@ def Amount_page(request, id):
         
 
 def Sell_page(request):
-    url = IP_ADDRESS+"/sell"
-
-
+    url = IP_ADDRESS+"/sell?id=*"
     response = requests.request("GET", url, headers=headers, data=payload)
-    return render(request, 'all.html', {'data':response.json()})
+    all_sum = 0
+    for i in response.json():
+        all_sum = i['all_sum']
+        break
+    return render(request, 'all.html', {'data':response.json(), 'all_sum':all_sum})
 
 
 def Sell_delete(request, id):
     url = IP_ADDRESS+"/sell?id="+id+""
-
     response = requests.request("DELETE", url, headers=headers, data=payload)
+    for i in response.json():
+        all_sum = i['all_sum']
+        break
+    return render(request, 'all.html', {'data':response.json(), 'all_sum':all_sum})
+
+def Sell_price(request, id):
+    if request.method == 'POST':
+        url = IP_ADDRESS+"/sell?id="+id+""
+        narx = request.POST.get('price')
+        dataa = {
+            'price':narx
+        }
+        response = requests.request("PUT", url, headers=headers, data=dataa)
+
+        return redirect('/sell_page')
+    else:
+        url = IP_ADDRESS+"/sell?id="+id+""
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return render(request, 'cost.html', {'data':response.json()})    
     
-    return render(request, 'all.html', {'data':response.json()})
-    
+def Selled(request, type):
+    tur = 'naqd'
+    if type == '1':
+        tur = 'plastik'
+        url = IP_ADDRESS+"/selled?type="+tur+""
+        response = requests.request("POST", url, headers=headers, data=payload)     
+        return redirect('/')
+    elif type == '2':
+        tur = 'naqd'
+        url = IP_ADDRESS+"/selled?type="+tur+""
+        response = requests.request("POST", url, headers=headers, data=payload)     
+        return redirect('/')
+    elif request.method == 'POST':
+        mijoz = request.POST.get('mijoz')
+        print(mijoz)
+        url = IP_ADDRESS+"/selled?type=nasiya&mijoz="+mijoz+""
+        response = requests.request("POST", url, headers=headers, data={'mijoz':mijoz})     
+        return redirect('/')
+
+
+
+def Customer_page(request):
+    url = IP_ADDRESS+"/customer"
+    response = requests.request("GET", url, headers=headers, data=payload)         
+    return render(request, 'customer.html', {'customers':response.json()})
+
+def Customer_add(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        comment = request.POST.get('comment')
+        payload1 = {
+            "name": name,
+            "phone": phone,
+            "comment": comment
+        }
+        url = IP_ADDRESS+"/customer"
+        response = requests.request("POST", url, headers=headers, data=payload1)         
+        return redirect('/nasiya')
+    else:
+        return render(request, 'customer_add.html')
